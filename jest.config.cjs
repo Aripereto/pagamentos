3e31fc20a654d5c48d6cf21599ec7119f0d88b7a
Aripereto <riri_pereto@hotmail.com>
2026-06-17 20:11:14 -0300

    ci: correção do integrated

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,16 +1,10 @@
 module.exports = {
   clearMocks: true,
-
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js'],
-
   coverageDirectory: 'reports/coverage',
-
   coverageProvider: 'v8',
-
   coverageReporters: ['text', 'lcov'],
-
   testEnvironment: 'node',
-
   testPathIgnorePatterns: ['/node_modules/', '/e2e/']
 };
